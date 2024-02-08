269a91f6f8fad611261e46fcb90a54c182c3c474
Debjyoti <djbabai.debjyoti@gmail.com>
2024-02-08 16:42:34 +0530

    initial commit

--- dataset-parser.py
+++ dataset-parser.py
@@ -8,12 +8,13 @@ label_data = []
 data_set_path = "data/insole-pressure"
 
 files = listdir(data_set_path)
-
+i = 1
 for file in files:
     full_file_path = path.join(data_set_path, file)
     mean_sensors = pd.read_csv(full_file_path, index_col=False)["Mean"].values.tolist()[:56]
     mean_data.append(mean_sensors)
-    label_data.append(file.split(".")[0])
+    label_data.append(i)
+    i = i + 1
 
 mean_sensor_data = np.array(mean_data)
 mean_sensor_label = np.array(label_data)
---
+++ insole-classifier.py
@@ -0,0 +1,84 @@
+import numpy as np
+import tensorflow as tf
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+
+data = np.load("training-data.npz", allow_pickle=True)
+
+x_data, y_data = data['out_x'], data['out_y']
+
+classes_values = ["a", "b", "c", "d"]
+classes = len(classes_values)
+
+y_data = tf.keras.utils.to_categorical(y_data - 1, classes)
+
+train_ratio = 0.70
+validation_ratio = 0.20
+test_ratio = 0.10
+
+x_train, x_test, y_train, y_test = train_test_split(x_data, y_data, test_size=1 - train_ratio)
+x_val, x_test, y_val, y_test = train_test_split(x_test, y_test, test_size=test_ratio/(test_ratio + validation_ratio))
+
+# Reshape input data
+x_train = x_train.reshape(x_train.shape[0], x_train.shape[1], 1)
+x_val = x_val.reshape(x_val.shape[0], x_val.shape[1], 1)
+x_test = x_test.reshape(x_test.shape[0], x_test.shape[1], 1)
+
+input_length = (x_train.shape[1], 1)
+
+train_dataset = tf.data.Dataset.from_tensor_slices((x_train, y_train))
+validation_dataset = tf.data.Dataset.from_tensor_slices((x_val, y_val))
+test_dataset = tf.data.Dataset.from_tensor_slices((x_test, y_test))
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Conv1D(8, kernel_size=3, activation='relu', padding='same', input_shape=input_length),
+    tf.keras.layers.MaxPooling1D(pool_size=2, strides=2, padding='same'),
+    tf.keras.layers.Dropout(0.25),
+    tf.keras.layers.Conv1D(16, kernel_size=3, activation='relu', padding='same'),
+    tf.keras.layers.MaxPooling1D(pool_size=2, strides=2, padding='same'),
+    tf.keras.layers.Dropout(0.25),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(classes, activation='softmax', name='y_pred')
+])
+
+model.compile(loss=tf.keras.losses.CategoricalCrossentropy(),
+              optimizer=tf.keras.optimizers.Adam(learning_rate=0.002), metrics=['acc'])
+
+# Batch size
+BATCH_SIZE = 10
+train_dataset = train_dataset.batch(BATCH_SIZE, drop_remainder=False)
+validation_dataset = validation_dataset.batch(BATCH_SIZE, drop_remainder=False)
+
+history = model.fit(train_dataset, epochs=5, validation_data=validation_dataset)
+
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+print(f"Training Accuracy: {round(np.average(acc), 3)}")
+print(f"Validation Accuracy: {round(np.average(val_acc), 3)}")
+
+predicted_labels = model.predict(x_test)
+actual_labels = y_test
+
+epochs = range(1, len(acc) + 1)
+fig, axs = plt.subplots(2, 1)
+
+# Plot loss
+axs[0].plot(epochs, loss, '-', label='Training loss')
+axs[0].plot(epochs, val_loss, 'b', label='Validation loss')
+axs[0].set_xlabel('Epoch')
+axs[0].set_ylabel('Loss')
+axs[0].grid(True)
+axs[0].legend(loc='best')
+
+# Plot accuracy
+axs[1].plot(epochs, acc, '-', label='Training acc')
+axs[1].plot(epochs, val_acc, 'b', label='Validation acc')
+axs[1].set_xlabel('Epoch')
+axs[1].set_ylabel('Accuracy')
+axs[1].grid(True)
+axs[1].legend(loc='best')
+
+plt.show()
